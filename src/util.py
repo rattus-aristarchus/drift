3a0f579c5941c21c36477608867f332535caa6c1
rattus-aristarchus <lankin.m.a@gmail.com>
2024-07-10 03:14:47 +0300

    worlds in separate files

--- src/util.py
+++ src/util.py
@@ -13,15 +13,28 @@ from kivy.logger import Logger
 
 # getcwd returns the current working directory, which can change during program execution,
 # and which can give different results depending on where the program is launched from. for
-# now, sys.path[0] is a better alternative, it gives the initial directory from which the script was launched
+# now, sys.path[0] is a better alternative, it gives the initial directory from which the
+# script was launched
 
 # MAIN_DIR = os.path.dirname(os.getcwd())
 MAIN_DIR = os.path.dirname(sys.path[0])
 Logger.info("Util: using dir " + MAIN_DIR)
 
 CONF = yaml.safe_load(open(MAIN_DIR + "/conf.yml", "r", encoding="utf-8"))
-CONST = yaml.safe_load(open(MAIN_DIR + "/const.yml", "r", encoding="utf-8"))
-WORLDS = yaml.safe_load(open(MAIN_DIR + "/worlds.yml", "r", encoding="utf-8"))
+DATA_DIR = os.path.join(MAIN_DIR, "res")
+CONST = yaml.safe_load(open(DATA_DIR + "/const.yml", "r", encoding="utf-8"))
+WORLDS_DIR = os.path.join(DATA_DIR, "worlds")
+WORLDS = {}
+
+for element in os.listdir(WORLDS_DIR):
+    element_path = os.path.join(WORLDS_DIR, element)
+    if os.path.isfile(element_path) and os.path.splitext(element)[1] == ".yml":
+        world = yaml.safe_load(open(element_path, "r", encoding="utf-8"))
+        world_name = os.path.splitext(element)[0]
+        WORLDS[world_name] = world
+
+print(WORLDS)
+
 # STRINGS = yaml.safe_load(open(MAIN_DIR + "/strings.yml", "r", encoding="utf-8"))
 
 
